195d922f7b52cf4a60af6941f86f4e184d6719bd
Muhammad <mhmmdbader9@gmail.com>
2020-09-29 14:41:30 +0300

    trse second

--- task.py
+++ task.py
@@ -3,9 +3,9 @@ import numpy as np
 from numpy.random import randn
 import matplotlib.pyplot as plt
 import seaborn as sns
-import random as rd
 
-# tests = []
+
+##### tests = []
 # print(rd.Random(4000))
 
 p = pd.DataFrame(randn(3, 4), columns="H M S, total_tests".split())
@@ -18,3 +18,4 @@ axis = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 sns.lmplot(x='total_tests', y='M', data=p)
 plt.show()
 
+## lost cause
